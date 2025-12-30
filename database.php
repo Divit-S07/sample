640502dd125703421052a1d0f7c7551202af96c4
Divit-S07 <sdivit2007@gmail.com>
2025-12-30 19:05:29 +0530

    fourth

--- database.php
+++ database.php
@@ -1,12 +1,9 @@
 <?php
 $conn = mysqli_connect("localhost", "root", " ", " ");
-$sql = "INSERT INTO students (name, email, department)
-VALUES ('Gayathri', 'gayathri@gmail.com', 'CSE')";
-if (mysqli_query($conn, $sql)) {
-
-echo "Record inserted!";
-} else {
-echo "Error: " . mysqli_error($conn);
+$sql = "SELECT * FROM students";
+$result = mysqli_query($conn, $sql);
+while ($row = mysqli_fetch_assoc($result)) {
+echo $row['id'] . " - " . $row['name'] . " - " . $row['email'] . "<br>";
 }
 mysqli_close($conn);
 ?>
