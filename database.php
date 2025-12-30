229f65ee979a5741cefeb739a8123e84896d15f3
Divit-S07 <sdivit2007@gmail.com>
2025-12-30 19:04:26 +0530

    third

--- database.php
+++ database.php
@@ -1,14 +1,12 @@
 <?php
 $conn = mysqli_connect("localhost", "root", " ", " ");
-$sql = "CREATE TABLE students (
-id INT,
-name VARCHAR(50),
-email VARCHAR(100),
-department VARCHAR(20)
-)";
+$sql = "INSERT INTO students (name, email, department)
+VALUES ('Gayathri', 'gayathri@gmail.com', 'CSE')";
 if (mysqli_query($conn, $sql)) {
-echo "Table created successfully!";
+
+echo "Record inserted!";
 } else {
 echo "Error: " . mysqli_error($conn);
+}
 mysqli_close($conn);
 ?>
