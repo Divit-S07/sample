680f0d979e2287d724d19a3bfa0f2e09bcae5f40
Divit-S07 <sdivit2007@gmail.com>
2025-12-30 19:00:12 +0530

    first

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+$conn = mysqli_connect("localhost", "root", " ", " ");
+$sql = "CREATE DATABASE school";
+if (mysqli_query($conn, $sql)) {
+echo "Database created successfully!";
+}
+else {
+echo "Error creating database: " . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
